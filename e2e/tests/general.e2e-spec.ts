00eb135521b2c18ad89fbe23c41bbda2368ac608
Anurag Rawat <anurag.rawat1@ge.com>
2023-05-31 15:03:49 +0100

    remove gh deploy action

--- e2e/tests/general.e2e-spec.ts
+++ e2e/tests/general.e2e-spec.ts
@@ -12,9 +12,10 @@ test('should click the button and open dialog', async (t) => {
     'test-form-dialog'
   );
 
-  const numberField = Selector('#formly_2_input__1')
+  const numberField = Selector('mat-label').withText('Test number').parent().child('formly-field-mat-input').child('input');
 
   await t.click(button);
+  await t.expect(numberField.visible).ok()
   await t.typeText(numberField, '3')
   await t.expect(formDialog.visible).eql(true)
 });
